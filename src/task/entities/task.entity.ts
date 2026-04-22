ba840c703c0e17edf7006dd754d57c58a1b6fb08
ygricks <cujbaigor90@gmail.com>
2026-04-22 23:06:56 +0300

    fix: entities

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,27 +1,46 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
 @Entity('tasks')
 export class Task {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column({ length: 500 })
-  name: string;
+  name!: string;
 
   @Column('text')
-  description: string;
+  description!: string;
 
   // status: 0 - pending, 1 - in progress, 2 - completed
   @Column({ default: 0 })
-  status: number;
-
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
+  status!: number;
 
   @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt!: Date;
+
+  @Column({
+    name: 'updated_at',
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
     onUpdate: 'CURRENT_TIMESTAMP',
   })
-  updatedAt: Date;
+  updatedAt!: Date;
+
+  @Column({ name: 'created_by', type: 'bigint' })
+  createdBy!: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'created_by' })
+  user!: User;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,25 +2,25 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id!: number;
 
   @Column({ length: 50, nullable: false })
-  name: string;
+  name!: string;
 
   @Column({ length: 100, nullable: false, unique: true })
-  email: string;
+  email!: string;
 
   @Column({ length: 100, nullable: false })
-  password: string;
+  password!: string;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  createdAt: Date;
+  createdAt!: Date;
 
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
     onUpdate: 'CURRENT_TIMESTAMP',
   })
-  updatedAt: Date;
+  updatedAt!: Date;
 }
